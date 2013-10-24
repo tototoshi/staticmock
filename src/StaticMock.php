8f6cf910022d95d45eb0e362ab681a7d58514f38
toshi <toshi@pixiv.com>
2013-10-24 10:11:19 +0900

    Added doc comment.

--- src/StaticMock.php
+++ src/StaticMock.php
@@ -8,7 +8,12 @@ use StaticMock\Stub;
  */
 class StaticMock {
 
-
+    /**
+     * Factory method to create a mock in one shot.
+     *
+     * @param $class_name
+     * @return Mock
+     */
     public static function mock($class_name)
     {
         return new Mock($class_name);
