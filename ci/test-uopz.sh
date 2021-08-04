ef26e932edab3ae4c986d3b20be1f2041fa11c82
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-08-04 09:33:52 +0900

    Fix the uopz version (#36)

--- ci/test-uopz.sh
+++ ci/test-uopz.sh
@@ -4,8 +4,10 @@ set -eux
 
 export PATH=$HOME/bin:$PATH
 
+uopz_version=$1
+
 $(dirname $0)/install_packages.sh
-$(dirname $0)/install_uopz.sh
+$(dirname $0)/install_uopz.sh $uopz_version
 
 ./composer install
 ./composer update
